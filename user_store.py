df95c8a0283e35211bf89a12d1e12a83f96933c2
Nicolas <nulysse4@gmail.com>
2014-02-26 20:53:29 -0500

    Added get test function

--- user_store.py
+++ user_store.py
@@ -17,14 +17,13 @@ class UserStore(object):
         self.operator_name = operator_name
         self.test_history = list()
 
-
         try:
             if not os.path.exists(self.get_store_file_directory()):
                 os.makedirs(self.get_store_file_directory())
 
         except BaseException as e:
             #TODO
-            print str(e)
+            print 'Cannot create directory {0!s} [{1!s}]'.format(self.get_store_file_directory(), str(e))
 
 
     def add_test(self, script_file, log_file, script_result):
@@ -33,24 +32,36 @@ class UserStore(object):
 
         """
 
-        #remove entry for same script name
         try:
+            #remove entry for same script name
             self.test_history = [test for test in self.test_history if not test['script']==script_file]
+
+            self.test_history.append({'timestamp': datetime.datetime.now(), 'script': script_file, 'log': log_file, 'result': script_result})
+
+            list.sort(self.test_history, key=lambda test_entry: test_entry['timestamp'], reverse=True)
+
+            self.test_history = self.test_history[0:_max_history]
+
+            self.save()
+
+        except BaseException as e:
+            #TODO
+            print 'Cannot add test to history [{0!s}]'.format(str(e))
+
+    def get_test(self, test_index):
+
+        try:
+
+            test_entry = self.test_history[test_index]
+            return test_entry['script'], test_entry['log']
+
         except:
-            pass
-
-        self.test_history.append({'timestamp': datetime.datetime.now(), 'script': script_file, 'log': log_file, 'result': script_result})
-
-        list.sort(self.test_history, key=lambda test_entry: test_entry['timestamp'], reverse=True)
-
-        self.test_history = self.test_history[0:_max_history]
-
-        self.save()
+            #TODO
+            print 'No test in history at index {0!s}'.format(test_index)
 
 
     def get_store_file_path(self):
         return os.path.join(self.get_store_file_directory(), self.operator_name + '.bin')
-
 
     def get_store_file_directory(self):
         return os.path.join(os.path.dirname(__file__), _store_path)
@@ -135,6 +146,7 @@ if __name__ == '__main__':
     store.load()
 
     store.print_history()
+
 
 
 
